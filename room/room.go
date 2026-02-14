a181b6864c7b0ceaf0f481a9e069ee63c2f96ac0
Mohammed Dayyan <mdayyanofficial@gmail.com>
2026-02-14 10:25:34 +0530

    Added graceful shutdown using os signals

--- room/room.go
+++ room/room.go
@@ -1,6 +1,8 @@
 package room
 
 import (
+	"log"
+
 	"github.com/MDAYYAN-007/whatsapp-lite/client"
 	"github.com/MDAYYAN-007/whatsapp-lite/models"
 )
@@ -10,6 +12,7 @@ type Room struct {
 	Broadcast chan models.Message
 	Join      chan *client.Client
 	Leave     chan *client.Client
+	Quit      chan struct{}
 }
 
 func NewRoom() *Room {
@@ -18,12 +21,20 @@ func NewRoom() *Room {
 		Broadcast: make(chan models.Message),
 		Join:      make(chan *client.Client),
 		Leave:     make(chan *client.Client),
+		Quit:      make(chan struct{}),
 	}
 }
 
 func (r *Room) Start() {
 	for {
 		select {
+
+		case <-r.Quit:
+			log.Println("Room shutting down")
+			for c := range r.Clients {
+				close(c.Send)
+			}
+			return
 
 		case c := <-r.Join:
 			r.Clients[c] = true
@@ -33,18 +44,45 @@ func (r *Room) Start() {
 				delete(r.Clients, c)
 				close(c.Send)
 			}
+			r.Broadcast <- models.Message{
+				SenderID: "system",
+				Content:  []byte(c.Username + " left the room"),
+			}
 
 		case msg := <-r.Broadcast:
-			for c := range r.Clients {
-				if c.ID != msg.SenderID {
-					select {
-					case c.Send <- msg.Content:
-					default:
-						close(c.Send)
-						delete(r.Clients, c)
+
+			var senderUsername string
+
+			if msg.SenderID == "system" {
+				senderUsername = "System"
+			} else {
+				for c := range r.Clients {
+					if c.ID == msg.SenderID {
+						senderUsername = c.Username
+						break
 					}
+				}
+			}
+
+			formatted := []byte("[" + senderUsername + "]: " + string(msg.Content))
+
+			for c := range r.Clients {
+
+				if c.ID == msg.SenderID {
+					continue
+				}
+
+				select {
+				case c.Send <- formatted:
+				default:
+					close(c.Send)
+					delete(r.Clients, c)
 				}
 			}
 		}
 	}
+}
+
+func (r *Room) Stop() {
+	close(r.Quit)
 }
